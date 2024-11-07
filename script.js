a73ad05e49a97eebab1bad90000b9171ba91c7cb
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-11-07 12:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+
+function Employee(name, age, jobTitle) {
+	// bcoz the parent class is not calling in a class
+	Person.call(this, name, age);
+	this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+}
+
+const person = new Person("Alice", 25);
+person.greet(); 
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
